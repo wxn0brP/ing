5fb6e09f4e2828749970ecf533c6ae8b1f210e03
wxn0brP <toja.filiper@gmail.com>
2026-03-25 22:04:21 +0100

    feat: aliases

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,13 @@ const [command, arg] = process.argv.slice(2);
 
 switch (command) {
     case "link":
+    case "l":
+    case "lnk":
         ing();
         break;
     case "install":
     case "i":
+    case "add":
         ingr(arg);
         break;
     case "u":
@@ -24,9 +27,10 @@ Unknown command: ${command}
 Available commands:
 
   link                Link package.bin to $HOME/.ing (Linux/MacOS)
+  Alias: l, lnk
 
   install [name]      Install/Update package from github.com/wxn0brP/[name]
-  Alias: i
+  Alias: i, add
   
   up [name]           Update packages
   Alias: u, update
